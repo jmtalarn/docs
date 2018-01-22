7dec65bf460c13ec8738850978b9463bb0084572
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-01-22 17:02:05 +0100

    Add fetch polyfill

--- next.config.js
+++ next.config.js
@@ -1,19 +1,29 @@
 const glob = require('glob-promise')
 
 module.exports = {
+  webpack: function(cfg) {
+    const originalEntry = cfg.entry
+    cfg.entry = async () => {
+      const entries = await originalEntry()
+      entries['main.js'].unshift('./polyfills.js')
+      return entries
+    }
+
+    return cfg
+  },
+
   exportPathMap: async function() {
     const paths = await glob('pages/**')
-    const pages = paths
-      .map(path => {
-        path = path.replace(/^pages/, '')
-        path = path.replace('index.js', '')
-        path = path.replace('.js', '')
-        
-        if (path === '/') return path
+    const pages = paths.map(path => {
+      path = path.replace(/^pages/, '')
+      path = path.replace('index.js', '')
+      path = path.replace('.js', '')
 
-        path = path.replace(/\/$/, '')
-        return path
-      })
+      if (path === '/') return path
+
+      path = path.replace(/\/$/, '')
+      return path
+    })
 
     const pageMap = pages.reduce((map, page) => {
       map[page] = { page }
