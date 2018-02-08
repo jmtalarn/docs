bfefea80ee233f4634d9772eaf231c736f62d00d
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-02-08 14:58:57 +0000

    Update next

--- next.config.js
+++ next.config.js
@@ -5,25 +5,27 @@ module.exports = {
     const originalEntry = cfg.entry
     cfg.entry = async () => {
       const entries = await originalEntry()
-      entries['main.js'].unshift('./polyfills.js')
+      if (entries['main.js']) entries['main.js'].unshift('./polyfills.js')
       return entries
     }
 
     return cfg
   },
-
   exportPathMap: async function() {
     const paths = await glob('pages/**')
-    const pages = paths.map(path => {
-      path = path.replace(/^pages/, '')
-      path = path.replace('index.js', '')
-      path = path.replace('.js', '')
+    const pages = paths
+      .map(path => {
+        path = path.replace(/^pages/, '')
+        path = path.replace('index.js', '')
+        path = path.replace('.js', '')
 
-      if (path === '/') return path
+        if (path === '/_document') return
+        if (path === '/') return path
 
-      path = path.replace(/\/$/, '')
-      return path
-    })
+        path = path.replace(/\/$/, '')
+        return path
+      })
+      .filter(Boolean)
 
     const pageMap = pages.reduce((map, page) => {
       map[page] = { page }
